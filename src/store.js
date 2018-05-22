c5136dbf9025d558926b13d4e52f44966576b4c9
GainorB <gainorbostwick@gmail.com>
2018-05-21 21:25:14 -0400

    initialState param is optional

--- src/store.js
+++ src/store.js
@@ -3,11 +3,9 @@ import thunk from 'redux-thunk';
 import promise from 'redux-promise';
 import rootReducer from './reducers';
 
-const initialState = {};
 const middleware = [thunk, promise];
 const store = createStore(
   rootReducer,
-  initialState,
   compose(appleMiddleware(...middleware)),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
